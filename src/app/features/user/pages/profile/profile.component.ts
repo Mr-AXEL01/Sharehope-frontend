09d1844cbc3a2b12bcf33f2476dc1b86d2239c25
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-16 15:02:16 +0000

    Feat(Profile): add profile component
    set up the profile componenet that will be the redirect for the user.

---
+++ src/app/features/user/pages/profile/profile.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {AuthService} from '../../../../core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.component.html',
+  standalone: true,
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+  username: string = 'Abdelhak';
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+}
