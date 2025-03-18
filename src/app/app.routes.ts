66e37333b83e0cf005023f4ec22735f9426c42cf
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-18 08:36:40 +0000

    Define routes for : authRoutes [login, register], dashboard , profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {authRoutes} from './features/auth';
+import {AdminGuard} from './core/guards/admin.guard';
+import {AuthGuard} from './core/guards/auth.guard';
+import {DashboardComponent} from './features/dashboard/pages/dashboard/dashboard.component';
+import {ProfileComponent} from './features/user/pages/profile/profile.component';
+import {NotAuthGuard} from './core/guards/not-auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: authRoutes,
+    canActivate: [NotAuthGuard],
+  },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard, AdminGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+];
