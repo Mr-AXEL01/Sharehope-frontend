2b1abe32f6215693abb28ec7b85bb6c22d376fa2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-20 23:46:44 +0000

    Feat(route): set up the route for the articles and a specefic article

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,55 @@ import {authRoutes} from './features/auth';
 import {AdminGuard} from './core/guards/admin.guard';
 import {AuthGuard} from './core/guards/auth.guard';
 import {DashboardComponent} from './features/dashboard/pages/dashboard/dashboard.component';
-import {ProfileComponent} from './features/user/pages/profile/profile.component';
 import {NotAuthGuard} from './core/guards/not-auth.guard';
+import {LayoutComponent} from './core/layout/pages/layout/layout.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/public/pages/home/home.component')
+            .then(m => m.HomeComponent)
+      },
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('./features/public/pages/about/about.component')
+            .then(m => m.AboutComponent)
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./features/user/pages/profile/profile.component')
+            .then(m => m.ProfileComponent),
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'articles',
+        loadComponent: () =>
+          import('./features/public/pages/articles/articles.component')
+            .then(m => m.ArticlesComponent)
+      },
+      {
+        path: 'articles/:id',
+        loadComponent: () =>
+          import('./features/public/pages/article-detail/article-detail.component')
+            .then(m => m.ArticleDetailComponent)
+      }
+    ]
+  },
   {
     path: 'auth',
     children: authRoutes,
     canActivate: [NotAuthGuard],
   },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard, AdminGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+  {
+    path: "**",
+    redirectTo: ""
+  }
 ];
