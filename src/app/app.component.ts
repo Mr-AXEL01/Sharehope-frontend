1a6904f8a82d315192364b1403900e8e6d4b88ea
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-03-20 07:03:15 +0000

    Refactor(app): use template instead of the templateUrl to avoid having too many file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LayoutComponent} from './core/layout/pages/layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, LayoutComponent],
+  template: `
+      <router-outlet />
+  `,
   standalone: true,
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'SHAREHOPE-FRONTEND';
